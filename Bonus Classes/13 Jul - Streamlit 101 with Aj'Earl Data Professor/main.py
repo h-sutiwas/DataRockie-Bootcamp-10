22cdfd6dc5cff799d074ca0975c7cb1210dc99b9
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2024-07-13 20:43:28 +0700

    altair library dataset & visualization

--- Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
+++ Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
-import numpy as np 
+import numpy as np
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
 
 st.title('🦖 My First Streamlit App')
 
@@ -16,3 +20,13 @@ else:
 st.header("Area Chart")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.area_chart(chart_data)
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
+
+c = alt.Chart(source).mark_circle(size=60).encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+    tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+).interactive()
