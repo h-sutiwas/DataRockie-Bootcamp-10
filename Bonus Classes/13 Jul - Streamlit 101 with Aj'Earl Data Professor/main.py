0cd532a3024a11d643edf7f4d0c69c42bd255866
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2024-07-13 20:26:01 +0700

    update streamlit

--- Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
+++ Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('My First Streamlit App')
+
+placeholder = st.empty()
+status = 1
+
+if status == 1:
+    placeholder.success("Success")
+else:
+    placeholder.error("Error")
