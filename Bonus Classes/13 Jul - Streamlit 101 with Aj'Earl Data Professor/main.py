1395a275b29439ad81875aa2f631714485bc308f
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2024-07-13 20:37:13 +0700

    update streamlit with chart

--- Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
+++ Bonus Classes/13 Jul - Streamlit 101 with Aj'Earl Data Professor/main.py
@@ -1,11 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np 
 
-st.title('My First Streamlit App')
+st.title('🦖 My First Streamlit App')
 
 placeholder = st.empty()
-status = 1
+status = st.radio("Select an option",
+                  ["Success", "Error"])
 
-if status == 1:
-    placeholder.success("Success")
+if status == "Success":
+    placeholder.success("Success !")
 else:
     placeholder.error("Error")
+
+st.header("Area Chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.area_chart(chart_data)
